ae1fbf7915036abf0eadf2e74e534fdfcf630054
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-07-13 13:02:06 -0300

    feat: configurar EF Core + SQLite e cascade delete entre Pessoa e Transacao

---
+++ backend/ControleGastos.Api/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Pessoa> Pessoas => Set<Pessoa>();
+    public DbSet<Transacao> Transacoes => Set<Transacao>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        modelBuilder.Entity<Transacao>()
+            .HasOne(t => t.Pessoa)
+            .WithMany(p => p.Transacoes)
+            .HasForeignKey(t => t.PessoaId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
