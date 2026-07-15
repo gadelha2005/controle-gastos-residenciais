2f6d596f6ac795c9b45654082281b1760d9263bd
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-07-15 12:27:07 -0300

    feat: configurar rotas do react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,20 @@
+import { BrowserRouter , Routes , Route , Navigate } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { PessoasPage } from "./pages/PessoasPage";
+import { TransacoesPage } from "./pages/TransacoesPage";
+import { TotaisPage } from "./pages/TotaisPage";
 
 export default function App(){
-  return <h1 className="text-3xl font-bold text-blue-600">Olá, Mundo!</h1>
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Navigate to="/pessoas" replace />} />
+          <Route path="pessoas" element={<PessoasPage />} />
+          <Route path="transacoes" element={<TransacoesPage />} />
+          <Route path="totais" element={<TotaisPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
